14b5c08c27daa23637a18e1f51ab0f0ed03f1358
Pretel1 <145733650+Pretel1@users.noreply.github.com>
2026-04-30 15:34:04 -0500

    Update supabase-config.js

--- js/supabase-config.js
+++ js/supabase-config.js
@@ -1,5 +1,5 @@
 // js/supabase-config.js
-import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm';
+import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2.49.4/+esm';
 
 const SUPABASE_URL = 'https://librysniqcicbdcobmzu.supabase.co';
 const SUPABASE_PUBLISHABLE_KEY = 'sb_publishable_OMlvkBnkhJOY1sZVIhS54Q_DENu2TpF';
@@ -15,7 +15,6 @@ export const supabase = createClient(SUPABASE_URL, SUPABASE_PUBLISHABLE_KEY, {
 export const LABS_BUCKET = 'laboratorios';
 export const MEDIA_BUCKET = 'media';
 
-// ⚠️ ESTA LÍNEA ES CLAVE
 export const EMAIL_FUNCTION_URL = 'https://librysniqcicbdcobmzu.supabase.co/functions/v1/send-email';
 
 console.log('✅ Supabase listo');
